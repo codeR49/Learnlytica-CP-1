f10f3163dd3565e604dd94c636a3902b61b4ffcb
ArnabC49 <arnabphukon.93@outlook.com>
2022-01-31 13:52:10 +0530

    Add admin auth  Weekly Monthly api

--- server/routes/weekmonthRouter.js
+++ server/routes/weekmonthRouter.js
@@ -4,17 +4,17 @@ const weekMonthController = require('../controllers/weekmonthController');
 const authenticate = require('../middlewares/authenticate');
 weekMonthRouter.use(express.json());
 
-weekMonthRouter.get('/getprobquizbymonth', weekMonthController.getProblemAssessmentByMonth);
+weekMonthRouter.get('/getprobquizbymonth', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getProblemAssessmentByMonth);
 
-weekMonthRouter.get('/getavgquizscorebymonth', weekMonthController.getAssessmentScoreByMonth);
+weekMonthRouter.get('/getavgquizscorebymonth', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getAssessmentScoreByMonth);
 
-weekMonthRouter.get('/getprobquizbymonthbyuser/:userid', weekMonthController.getProblemAssessmentByMonthByUser);
+weekMonthRouter.get('/getprobquizbymonthbyuser/:userid', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getProblemAssessmentByMonthByUser);
 
-weekMonthRouter.get('/getprobquizbyweek', weekMonthController.getProblemAssessmentByWeek);
+weekMonthRouter.get('/getprobquizbyweek', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getProblemAssessmentByWeek);
 
-weekMonthRouter.get('/getavgquizscorebyweek', weekMonthController.getAssessmentScoreByWeek);
+weekMonthRouter.get('/getavgquizscorebyweek', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getAssessmentScoreByWeek);
 
-weekMonthRouter.get('/getprobquizbyweekbyuser/:userid', weekMonthController.getProblemAssessmentByWeekByUser);
+weekMonthRouter.get('/getprobquizbyweekbyuser/:userid', authenticate.verifyUser, authenticate.verifyAdmin, weekMonthController.getProblemAssessmentByWeekByUser);
 
 module.exports = weekMonthRouter;
 
